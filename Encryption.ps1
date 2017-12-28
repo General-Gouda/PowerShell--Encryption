4cdc3c74bfebdd4099b761076bf23423039a69cd
Matt Marchese <matthew.marchese1@gmail.com>
2017-12-28 15:02:13 -0500

    Encryption.ps1 changes: - Moved New-SelfSignedCert and Encrypt-Asymmetric functions out of the New-EncryptedString function for script clarity.

--- Encryption.ps1
+++ Encryption.ps1
@@ -28,7 +28,7 @@ function Get-DecryptedPassword
 			Write-Warning "Failure decrypting key.`nError:$_"
 			$adminCredential = $null
 		}
-	} 
+	}
 	else
 	{
 		Write-Warning "Certificate needed for password decryption not found.`nError:$_"
@@ -61,7 +61,7 @@ function Get-EncryptedString
 		{
 			Write-Host "Failure decrypting key. Terminating script."
 		}
-	} 
+	}
 	else
 	{
 		Write-Host "Certificate needed for string decryption not found. Terminating script."
@@ -70,8 +70,207 @@ function Get-EncryptedString
 	return $plainText
 }
 
+function New-SelfSignedCert
+{
+	[CmdletBinding()]
+	param
+	(
+		[ValidateSet("Personal", "LocalMachine")]
+		[string]$CertificateStore = "LocalMachine",
+
+		[string]$CertificateSubject,
+
+		[ValidateSet("SHA256", "SHA1")]
+		[string]$SignatureAlgorithm = "SHA256",
+
+		[int]$CertificateExpirationInYears = 30,
+
+		[int]$CertificateLength = 2048,
+
+		<#
+		For CertificateExportable
+		0 = No private key export
+		1 = Private key can be exported
+		2 = Private key can be exported in plaintext
+		4 = Private key can be exported once for archiving
+		8 = Private key can be exported once in plaintext for archiving
+		#>
+		[ValidateSet(0,1,2,4,8)]
+		[int]$CertificateExportable = 1,
+
+		[switch]$AddServerAuth,
+
+		[switch]$AddClientAuth,
+
+		[switch]$AddSmartCardAuth,
+
+		[switch]$AddEncryptedFileSystem,
+
+		[switch]$AddCodeSigning
+	)
+
+	$ErrorActionPreference = "Stop"
+
+	#----------------------------------------------------#
+	# Checks to make certain PowerShell is running as Administrator
+	#----------------------------------------------------#
+	if ((([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) -eq $false) {
+		Write-Host "The PowerShell session is not running as an administrator. Exiting script in 10 seconds."
+		Start-Sleep -Seconds 10
+		Exit
+	}
+
+	If ($CertificateStore -eq "Personal")
+	{
+		$machineContext = 0
+		$initContext = 1
+	}
+	ElseIF ($CertificateStore -eq "LocalMachine")
+	{
+		$machineContext = 1
+		$initContext = 2
+	}
+
+	$name = New-Object -com "X509Enrollment.CX500DistinguishedName.1"
+
+	$name.Encode("CN=$CertificateSubject", 0)
+
+	$key = New-Object -com "X509Enrollment.CX509PrivateKey.1"
+
+	$key.ProviderName = "Microsoft RSA SChannel Cryptographic Provider"
+
+	$key.KeySpec = 1
+
+	$key.Length = $CertificateLength
+
+	$key.SecurityDescriptor = "D:PAI(A;;0xd01f01ff;;;SY)(A;;0xd01f01ff;;;BA)(A;;0x80120089;;;NS)"
+
+	$key.MachineContext = $machineContext
+
+	$key.ExportPolicy = $CertificateExportable
+
+	$key.Create()
+
+	$ekuoids = New-Object -com "X509Enrollment.CObjectIds.1"
+
+	If ($AddServerAuth -eq $true)
+	{
+		$serverauthoid = New-Object -com "X509Enrollment.CObjectId.1"
+
+		$serverauthoid.InitializeFromValue("1.3.6.1.5.5.7.3.1")
+
+		$ekuoids.add($serverauthoid)
+	}
+
+	If ($AddClientAuth -eq $true)
+	{
+		$clientauthoid = New-Object -com "X509Enrollment.CObjectId.1"
+
+		$clientauthoid.InitializeFromValue("1.3.6.1.5.5.7.3.2")
+
+		$ekuoids.add($clientauthoid)
+	}
+
+	If ($AddSmartCardAuth -eq $true)
+	{
+		$smartcardoid = New-Object -com "X509Enrollment.CObjectId.1"
+
+		$smartcardoid.InitializeFromValue("1.3.6.1.4.1.311.20.2.2")
+
+		$ekuoids.add($smartcardoid)
+	}
+
+	If ($AddEncryptedFileSystem -eq $true)
+	{
+		$efsoid = New-Object -com "X509Enrollment.CObjectId.1"
+
+		$efsoid.InitializeFromValue("1.3.6.1.4.1.311.10.3.4")
+
+		$ekuoids.add($efsoid)
+	}
+
+	If ($AddCodeSigning -eq $true)
+	{
+		$codesigningoid = New-Object -com "X509Enrollment.CObjectId.1"
+
+		$codesigningoid.InitializeFromValue("1.3.6.1.5.5.7.3.3")
+
+		$ekuoids.add($codesigningoid)
+	}
+
+	$SigOID = New-Object -ComObject X509Enrollment.CObjectId
+
+	$SigOID.InitializeFromValue(([Security.Cryptography.Oid]$SignatureAlgorithm).Value)
+
+	$ekuext = New-Object -ComObject "X509Enrollment.CX509ExtensionEnhancedKeyUsage.1"
+
+	$ekuext.InitializeEncode($ekuoids)
+
+	$cert = New-Object -ComObject "X509Enrollment.CX509CertificateRequestCertificate.1"
+
+	$cert.InitializeFromPrivateKey($initContext, $key, "")
+
+	$cert.Subject = $name
+
+	$cert.Issuer = $cert.Subject
+
+	$cert.NotBefore = Get-Date
+
+	$cert.NotAfter = $cert.NotBefore.AddYears($CertificateExpirationInYears)
+
+	$cert.X509Extensions.Add($ekuext)
+
+	$cert.SignatureInformation.HashAlgorithm = $SigOID
+
+	$cert.Encode()
+
+	$enrollment = New-Object -ComObject "X509Enrollment.CX509Enrollment.1"
+
+	$enrollment.InitializeFromRequest($cert)
+
+	$certdata = $enrollment.CreateRequest(0)
+
+	$enrollment.InstallResponse(2, $certdata, 0, "")
+
+	Write-Host "`nFinished" -ForegroundColor Green
+
+	Return (Get-ChildItem Cert:\$CertificateStore\My | Where {$_.Subject -eq "CN=$CertificateSubject"}).Thumbprint
+}
+
+Function Encrypt-Asymmetric
+{
+	[CmdletBinding()]
+	[OutputType([System.String])]
+	param(
+		[Parameter(Position=0, Mandatory=$true)][ValidateNotNullOrEmpty()][System.String]
+		$ClearText,
+		[Parameter(Position=1, Mandatory=$true)][ValidateNotNullOrEmpty()][ValidateScript({Test-Path $_ -PathType Leaf})][System.String]
+		$PublicCertFilePath
+	)
+
+	$secureString = $ClearText | ConvertTo-SecureString -AsPlainText -Force
+	$PublicCert = Get-Item -Path $PublicCertFilePath -ErrorAction Stop
+
+	$key = New-Object byte[](32)
+	$rng = [System.Security.Cryptography.RNGCryptoServiceProvider]::Create()
+
+	$rng.GetBytes($key)
+
+	$encryptedString = ConvertFrom-SecureString -SecureString $secureString -Key $key
+	$EncryptedByteArray = $PublicCert.PublicKey.Key.Encrypt($key,$true)
+
+	$encryptedInfo = [PSCustomObject]@{
+		"EncryptedString" = $encryptedString
+		"EncryptedKeyInBase64" = $EncryptedByteArray
+		"Thumbprint" = $PublicCert.Thumbprint
+	}
+
+	Return $encryptedInfo
+}
+
+
 #----------------------------------------------------#
-# String Encryption Script 
+# String Encryption Script
 #----------------------------------------------------#
 function New-EncryptedString
 {
@@ -158,204 +357,6 @@ function New-EncryptedString
 
 	)
 
-	function New-SelfSignedCert
-	{
-		[CmdletBinding()]
-		param
-		(
-			[ValidateSet("Personal", "LocalMachine")]
-			[string]$CertificateStore = "LocalMachine",
-
-			[string]$CertificateSubject,
-
-			[ValidateSet("SHA256", "SHA1")]
-			[string]$SignatureAlgorithm = "SHA256",
-
-			[int]$CertificateExpirationInYears = 30,
-
-			[int]$CertificateLength = 2048,
-
-			<# 
-			For CertificateExportable
-			0 = No private key export
-			1 = Private key can be exported
-			2 = Private key can be exported in plaintext
-			4 = Private key can be exported once for archiving
-			8 = Private key can be exported once in plaintext for archiving
-			#>
-			[ValidateSet(0,1,2,4,8)]
-			[int]$CertificateExportable = 1, 
-
-			[switch]$AddServerAuth,
-  
-			[switch]$AddClientAuth,
-  
-			[switch]$AddSmartCardAuth,
-  
-			[switch]$AddEncryptedFileSystem,
-  
-			[switch]$AddCodeSigning
-		)
-
-		$ErrorActionPreference = "Stop" 
-  
-		#----------------------------------------------------#
-		# Checks to make certain PowerShell is running as Administrator
-		#----------------------------------------------------#
-		if ((([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) -eq $false) {
-			Write-Host "The PowerShell session is not running as an administrator. Exiting script in 10 seconds."
-			Start-Sleep -Seconds 10
-			Exit
-		}
-
-		If ($CertificateStore -eq "Personal")
-		{
-			$machineContext = 0
-			$initContext = 1
-		}
-		ElseIF ($CertificateStore -eq "LocalMachine")
-		{
-			$machineContext = 1
-			$initContext = 2
-		}
-  
-		$name = New-Object -com "X509Enrollment.CX500DistinguishedName.1" 
-  
-		$name.Encode("CN=$CertificateSubject", 0)
-  
-		$key = New-Object -com "X509Enrollment.CX509PrivateKey.1" 
-  
-		$key.ProviderName = "Microsoft RSA SChannel Cryptographic Provider" 
-  
-		$key.KeySpec = 1
-  
-		$key.Length = $CertificateLength
-  
-		$key.SecurityDescriptor = "D:PAI(A;;0xd01f01ff;;;SY)(A;;0xd01f01ff;;;BA)(A;;0x80120089;;;NS)" 
-  
-		$key.MachineContext = $machineContext
-  
-		$key.ExportPolicy = $CertificateExportable
-  
-		$key.Create()
-  
-		$ekuoids = New-Object -com "X509Enrollment.CObjectIds.1" 
-  
-		If ($AddServerAuth -eq $true)
-		{
-			$serverauthoid = New-Object -com "X509Enrollment.CObjectId.1" 
-  
-			$serverauthoid.InitializeFromValue("1.3.6.1.5.5.7.3.1")
-  
-			$ekuoids.add($serverauthoid)
-		}
-  
-		If ($AddClientAuth -eq $true)
-		{
-			$clientauthoid = New-Object -com "X509Enrollment.CObjectId.1" 
-  
-			$clientauthoid.InitializeFromValue("1.3.6.1.5.5.7.3.2")
-  
-			$ekuoids.add($clientauthoid)
-		}
-  
-		If ($AddSmartCardAuth -eq $true)
-		{
-			$smartcardoid = New-Object -com "X509Enrollment.CObjectId.1" 
-  
-			$smartcardoid.InitializeFromValue("1.3.6.1.4.1.311.20.2.2")
-  
-			$ekuoids.add($smartcardoid)
-		}
-  
-		If ($AddEncryptedFileSystem -eq $true)
-		{
-			$efsoid = New-Object -com "X509Enrollment.CObjectId.1" 
-  
-			$efsoid.InitializeFromValue("1.3.6.1.4.1.311.10.3.4")
-  
-			$ekuoids.add($efsoid)
-		}
-  
-		If ($AddCodeSigning -eq $true)
-		{
-			$codesigningoid = New-Object -com "X509Enrollment.CObjectId.1" 
-  
-			$codesigningoid.InitializeFromValue("1.3.6.1.5.5.7.3.3")
-  
-			$ekuoids.add($codesigningoid)
-		}
-	
-		$SigOID = New-Object -ComObject X509Enrollment.CObjectId
-
-		$SigOID.InitializeFromValue(([Security.Cryptography.Oid]$SignatureAlgorithm).Value)
-
-		$ekuext = New-Object -ComObject "X509Enrollment.CX509ExtensionEnhancedKeyUsage.1"
-  
-		$ekuext.InitializeEncode($ekuoids)
-  
-		$cert = New-Object -ComObject "X509Enrollment.CX509CertificateRequestCertificate.1" 
-  
-		$cert.InitializeFromPrivateKey($initContext, $key, "")
-  
-		$cert.Subject = $name 
-  
-		$cert.Issuer = $cert.Subject
-  
-		$cert.NotBefore = Get-Date 
-  
-		$cert.NotAfter = $cert.NotBefore.AddYears($CertificateExpirationInYears)
-  
-		$cert.X509Extensions.Add($ekuext)
-
-		$cert.SignatureInformation.HashAlgorithm = $SigOID
-  
-		$cert.Encode()
-  
-		$enrollment = New-Object -ComObject "X509Enrollment.CX509Enrollment.1" 
-  
-		$enrollment.InitializeFromRequest($cert)
-  
-		$certdata = $enrollment.CreateRequest(0)
-  
-		$enrollment.InstallResponse(2, $certdata, 0, "")
-  
-		Write-Host "`nFinished" -ForegroundColor Green
-
-		Return (Get-ChildItem Cert:\$CertificateStore\My | Where {$_.Subject -eq "CN=$CertificateSubject"}).Thumbprint
-	}
-
-	Function Encrypt-Asymmetric
-	{
-		[CmdletBinding()]
-		[OutputType([System.String])]
-		param(
-			[Parameter(Position=0, Mandatory=$true)][ValidateNotNullOrEmpty()][System.String]
-			$ClearText,
-			[Parameter(Position=1, Mandatory=$true)][ValidateNotNullOrEmpty()][ValidateScript({Test-Path $_ -PathType Leaf})][System.String]
-			$PublicCertFilePath
-		)
-
-		$secureString = $ClearText | ConvertTo-SecureString -AsPlainText -Force
-		$PublicCert = Get-Item -Path $PublicCertFilePath -ErrorAction Stop
-
-		$key = New-Object byte[](32)
-		$rng = [System.Security.Cryptography.RNGCryptoServiceProvider]::Create()
-
-		$rng.GetBytes($key)
-
-		$encryptedString = ConvertFrom-SecureString -SecureString $secureString -Key $key
-		$EncryptedByteArray = $PublicCert.PublicKey.Key.Encrypt($key,$true)
-
-		$encryptedInfo = [PSCustomObject]@{
-			"EncryptedString" = $encryptedString
-			"EncryptedKeyInBase64" = $EncryptedByteArray
-			"Thumbprint" = $PublicCert.Thumbprint
-		}
- 
-		Return $encryptedInfo
-	}
-
 	if ($PSCmdlet.ParameterSetName -eq "New") {
 		$newCertificateParams = @{
 			CertificateStore = $CertStoreLocation
@@ -396,13 +397,13 @@ function New-EncryptedString
 		$newCertificate = Get-ChildItem $CertStoreLocation\$CertThumbprint
 	}
 
-	$StringInfo = Encrypt-Asymmetric -ClearText $StringToEncrypt -PublicCertFilePath "$CertStoreLocation\$($newCertificate.Thumbprint)" 
+	$StringInfo = Encrypt-Asymmetric -ClearText $StringToEncrypt -PublicCertFilePath "$CertStoreLocation\$($newCertificate.Thumbprint)"
 
 	$StringOutput = [PSCustomObject]@{
 		EncryptedKeyInBase64 = [Convert]::ToBase64String($StringInfo.EncryptedKeyInBase64) # byte array to base64 string
 		EncryptedString = $StringInfo.EncryptedString
 		Thumbprint = $StringInfo.Thumbprint
-	} 
+	}
 
 	$StringOutput | Export-Csv $OutputLocation\$FileName -NoTypeInformation
 
